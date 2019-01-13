536df3812277eb61276cafcd2c8c7c90a2877397
Bakudankun <bakudankun@gmail.com>
2019-01-13 23:49:39 +0900

    Pythonを3.7.0にアップデート
    2to3（全fixer使用）でコードをコンバート

--- vocalonobis2mylist.py
+++ vocalonobis2mylist.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
-import sys, codecs, re, cgi, urllib, urllib2, cookielib, xml.dom.minidom, time, json, os
+import sys, codecs, re, cgi, urllib.request, urllib.parse, urllib.error, urllib.request, urllib.error, urllib.parse, http.cookiejar, xml.dom.minidom, time, json, os
 userid=os.environ.get("V2M_USERID")
 passwd=os.environ.get("V2M_PASSWD")
 
 
 def getToken():
-    html = urllib2.urlopen("http://www.nicovideo.jp/my/mylist").read()
+    html = urllib.request.urlopen("http://www.nicovideo.jp/my/mylist").read()
     for line in html.splitlines():
-        mo = re.match(r'^\s*NicoAPI\.token = "(?P<token>[\d\w-]+)";\s*',line)
+        mo = re.match(r'^\s*NicoAPI\.token = "(?P<token>[\d\w-]+)";\s*', line)
         if mo:
             token = mo.group('token')
             break
@@ -16,8 +16,8 @@ def getToken():
     return token
 
 
-def addvideo_tomylist(mid,item,desc):
-    print "adding rank %s: %s\t%s" % (desc, item["smid"], item["title"])
+def addvideo_tomylist(mid, item, desc):
+    print("adding rank %s: %s\t%s" % (desc, item["smid"], item["title"]))
     cmdurl = "http://www.nicovideo.jp/api/mylist/add"
     q = {}
     q['group_id'] = mid
@@ -25,19 +25,19 @@ def addvideo_tomylist(mid,item,desc):
     q['item_id'] = item["smid"]
     q['description'] = desc
     q['token'] = token
-    cmdurl += "?" + urllib.urlencode(q)
-    j = json.load( urllib2.urlopen(cmdurl), encoding='utf8')
+    cmdurl += "?" + urllib.parse.urlencode(q)
+    j = json.load( urllib.request.urlopen(cmdurl), encoding='utf8')
 
 
 def clear_mylist(mid):
-    j = json.load(urllib2.urlopen("http://www.nicovideo.jp/api/mylist/list?group_id=" + str(mid)), encoding='utf8')
+    j = json.load(urllib.request.urlopen("http://www.nicovideo.jp/api/mylist/list?group_id=" + str(mid)), encoding='utf8')
     id_list = []
     for item in j['mylistitem']:
         id_list.append(item['item_id'])
     cmdurl = "http://www.nicovideo.jp/api/mylist/delete?group_id=%s&token=%s" % ( mid, token )
     for item_id in id_list:
-        cmdurl += "&" + urllib.quote_plus( "id_list[0][]" ) + "=%s" % item_id
-    k = json.load( urllib2.urlopen(cmdurl), encoding='utf8')
+        cmdurl += "&" + urllib.parse.quote_plus( "id_list[0][]" ) + "=%s" % item_id
+    k = json.load( urllib.request.urlopen(cmdurl), encoding='utf8')
 
 
 def getRanking(mode):
@@ -50,7 +50,7 @@ def getRanking(mode):
     else :
         sys.exit(1)
 
-    rss = urllib2.urlopen("http://vocalonobis.com/feed/?type=" + type + "&pages=1")
+    rss = urllib.request.urlopen("http://vocalonobis.com/feed/?type=" + type + "&pages=1")
     dom = xml.dom.minidom.parse(rss)
     rank = []
     for item in dom.getElementsByTagName("item"):
@@ -68,7 +68,7 @@ if __name__ == "__main__" :
     argc = len(argv)
 
     if argc < 2 :
-        print "error: no mylist given"
+        print("error: no mylist given")
         sys.exit(1)
     else :
         mid = argv[1]
@@ -78,17 +78,17 @@ if __name__ == "__main__" :
         elif argv[2] in ["daily", "weekly", "monthly"] :
             mode = argv[2]
         else :
-            print "invalid mode: %s" % argv[2]
+            print("invalid mode: %s" % argv[2])
             sys.exit(1)
 
     #ランキング取得
     rank = getRanking(mode)
 
     #ログイン
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()))
-    urllib2.install_opener(opener)
-    urllib2.urlopen("https://secure.nicovideo.jp/secure/login",
-                    urllib.urlencode( {"mail":userid, "password":passwd}) )
+    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(http.cookiejar.CookieJar()))
+    urllib.request.install_opener(opener)
+    urllib.request.urlopen("https://secure.nicovideo.jp/secure/login",
+                    urllib.parse.urlencode( {"mail":userid, "password":passwd}) )
 
     #トークン取得
     token = getToken()
@@ -97,6 +97,6 @@ if __name__ == "__main__" :
     clear_mylist(mid)
 
     #マイリストに動画を登録
-    for i,item in enumerate(rank):
+    for i, item in enumerate(rank):
         addvideo_tomylist(mid, item, str(i+1).zfill(3) )
         time.sleep(1)
